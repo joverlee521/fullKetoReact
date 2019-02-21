0c10e3fe704f78a7cc9b4cbd5e7fa02f6fdfe38d
Jover Lee <joverlee521@gmail.com>
2019-02-21 15:40:31 -0800

    added expansion panel to recipe cards to reveal nutrition facts

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid, Card, CardMedia, CardContent, CardActions, Button, Typography, Divider, withStyles } from "@material-ui/core";
+import { Grid, Card, CardMedia, CardContent, CardActions, 
+    Button, Typography, Divider, 
+    ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, 
+    withStyles } from "@material-ui/core";
 import { orange } from "@material-ui/core/colors";
 
 const styles = {
@@ -18,10 +21,13 @@ const styles = {
     },
     recipeImage: {
         width: "100%",
-        flex: "1 1 400px"
+        flex: "1 1 400px",
+        backgroundSize: "contain"
     },
     cardContent: {
-        flex: "1 0 auto"
+        flex: "1 0 auto",
+        padding: 0,
+        width: "100%",
     },
     cardLink: {
         textDecoration: "none",
@@ -29,17 +35,49 @@ const styles = {
     },
     divider: {
         width: "100%"
+    },
+    expansion: {
+        display: "flex",
+        flexDirection: "column",
+        boxShadow: "none !important"
+    },
+    panelSummary: {
+        alignSelf: "center"
+    },
+    title: {
+        padding: "0px !important"
+    },
+    panelDetails: {
+        flexDirection: "column"
     }
 };
 
 function RecipeCard(props){
     const { classes, recipe } = props;
+    const totalCarb = Math.floor(recipe.totalNutrients.CHOCDF.quantity / recipe.yield);
+    const fiber = Math.floor(recipe.totalNutrients.FIBTG.quantity / recipe.yield);
+    const netCarb = totalCarb - fiber;
     return(
         <Grid item xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } className={ classes.container }>
             <Card className={ classes.card }>
                 <CardMedia image={ recipe.image } className={ classes.recipeImage }/>
                 <CardContent className={ classes.cardContent }>
-                    <Typography variant="title">{ recipe.label }</Typography>
+                    <ExpansionPanel className={ classes.expansion }>
+                        <ExpansionPanelSummary classes={ { root: classes.panelSummary } }>
+                            <Typography variant="title" className={ classes.title }>{ recipe.label }</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails className={ classes.panelDetails }>
+                            <Typography variant="title">Nutrition Facts</Typography>
+                            <Typography variant="subtitle1">Servings: { recipe.yield }</Typography>
+                            <Typography variant="subtitle2">Per Serving: </Typography>
+                            <Typography>Calories: { Math.floor(recipe.calories / recipe.yield) } kcal</Typography>
+                            <Typography>Fat: { Math.floor(recipe.totalNutrients.FAT.quantity / recipe.yield) } g</Typography>
+                            <Typography>Protein: { Math.floor(recipe.totalNutrients.PROCNT.quantity / recipe.yield) } g</Typography>
+                            <Typography>Total Carb: { totalCarb } g</Typography>
+                            <Typography>Fiber: { fiber } g</Typography>
+                            <Typography>Net Carb: { netCarb } g</Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
                 </CardContent>
                 <Divider className={ classes.divider } light/>
                 <CardActions>
@@ -50,6 +88,7 @@ function RecipeCard(props){
             </Card>
         </Grid>
     );
+    
 }
 
 RecipeCard.propTypes = {
